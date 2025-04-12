304a27ae13f45e82a682bf4bc9f4f86d59c9c9d1
fcogboji <136510485+fcogboji@users.noreply.github.com>
2025-04-12 11:53:44 +0100

    Update page.tsx
    about page updated

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,7 +4,17 @@ export default function About() {
   return (
     <div>
         <Link className="text-blue-500" href="/">Go to home</Link>
-      About
+                About
+                <p>Our friendly team’s skill-set combined with our vetted supply chain of proven expert professionals and consultants, has built an excellent reputation for providing our clients with first-time, on-time and on-budget project deliveries.
+          
+                We engage in clear, concise reporting processes throughout the life cycle of a project, from inception and issue of licenses, through the planning stage, design and installation to completion ready for occupancy.
+                
+                We know that the success of our company is dependent on the success of our delivery, whatever the size or requirements of the project. We work very closely with our clients and fully engage on their specific requirements to ensure goals are achieved and 100% satisfaction.
+                
+                Whether it’s a refurb of your existing space or a move to a completely new building, we’ve got you. From CAT A, ready-to-work offices to full CAT B fit-out we can help. Our job is to make your project as stress-free as possible - we will look after the design, management, and delivery for you.
+                
+                Sound too good to be true?
+We promise you, it’s not.</p>
     </div>
   );
 }
